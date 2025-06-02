73f6310331fffd152fb861eea7984ca2002475d0
David Leung <265220+dhl@users.noreply.github.com>
2025-06-02 16:31:46 +0800

    chore: Move contexts out of app folder
    Reduces confusion when looking at content and contexts in the same folder

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,7 +7,7 @@ import "@/app/globals.css";
 import Header from "@/app/components/Header/Header";
 import Footer from "@/app/components/Footer/Footer";
 import GlobalModals from "@/app/components/Modals/GlobalModals";
-import WalletProvider from "@/app/contexts/WalletProvider";
+import WalletProvider from "@/contexts/WalletProvider";
 import { Geist_Mono, Funnel_Display } from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google'
 
