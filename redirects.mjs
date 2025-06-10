0c798f9446e4f0cbdd8c095e3e39660e3afec91a
David Leung <265220+dhl@users.noreply.github.com>
2025-06-10 14:51:13 +0800

    fix: set permanence of the redirects explicitly
    (cherry picked from commit bd658da73476faa0d5f67e5a00d70df1726d45b7)

--- redirects.mjs
+++ redirects.mjs
@@ -2,14 +2,17 @@ const redirects = [
   {
     source: "/:locale/courses/anchor-vault/lesson",
     destination: "/:locale/courses/anchor-vault",
+    permanent: false,
   },
   {
     source: "/:locale/courses/anchor-escrow/lesson",
     destination: "/:locale/courses/anchor-escrow",
+    permanent: false,
   },
   {
     source: "/:locale/courses/pinocchio-vault/lesson",
     destination: "/:locale/courses/pinocchio-vault",
+    permanent: false,
   },
 ];
 
