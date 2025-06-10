cac68ce3fa73f253d7d179f6df9a9d6ef00dade7
David Leung <265220+dhl@users.noreply.github.com>
2025-06-10 14:47:08 +0800

    feat: redirect old course URL to course page
    (cherry picked from commit 86c1ccda49f7e98a5006d3297bf0aa0f9234121f)

---
+++ redirects.mjs
@@ -0,0 +1,16 @@
+const redirects = [
+  {
+    source: "/:locale/courses/anchor-vault/lesson",
+    destination: "/:locale/courses/anchor-vault",
+  },
+  {
+    source: "/:locale/courses/anchor-escrow/lesson",
+    destination: "/:locale/courses/anchor-escrow",
+  },
+  {
+    source: "/:locale/courses/pinocchio-vault/lesson",
+    destination: "/:locale/courses/pinocchio-vault",
+  },
+];
+
+export default redirects; 
